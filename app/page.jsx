f2ec8fbb20b959ca9e06cc04ae7c2c44226ce946
thiagoferreira564 <thiago.rodrigues@sp.senai.br>
2023-09-20 08:46:21 -0300

    header and footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import styles from "./page.module.css";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <main>
+      <main className={styles.divMain}>
         <h1>Teste</h1>
       </main>
+      <Footer/>
     </div>
   );
 };
